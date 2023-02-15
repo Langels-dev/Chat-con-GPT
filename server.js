46b052d1e42a666074e6f4f3023554af5525fd92
Luis Jimenez <82353789+Langels-dev@users.noreply.github.com>
2023-02-15 17:33:26 -0400

    Change code

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const port = 8000;
 
 app.use(express.static('public'));
 
@@ -20,6 +21,6 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(8000, () => {
-  console.log('listening on *:3000');
+http.listen(port, () => {
+  console.log('listening on *:' + port);
 });
